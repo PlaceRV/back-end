1152341a92c89b2d77ce23bce235289b11167738
TakahashiNguyen <83152264+TakahashiNguyen@users.noreply.github.com>
2024-08-02 09:01:03 +0700

    update 2 files and delete 1 file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { WebSocketServer } from 'ws';
-import { useServer } from 'graphql-ws/lib/use/ws';
-declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
