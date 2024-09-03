eb324b3d66a67da85550511e96c615a68f05f2e1
takahashinguyen <vietuk.nguyen.git@icloud.com>
2024-09-03 20:10:31 +0700

    update .eslintrc.js and TestFormatLint.yaml

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
 		node: true,
 		jest: true,
 	},
-	ignorePatterns: ['*.js', 'dist/*', '*.d.ts'],
+	ignorePatterns: ['*.js', 'dist/*', '*.d.ts', '*.config.*'],
 	rules: {
 		'@typescript-eslint/interface-name-prefix': 'off',
 		'@typescript-eslint/explicit-function-return-type': 'off',
